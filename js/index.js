d1d7244464de440d2c71d97b1bcc0482ccc6075b
prince475 <danielmwai475@gmail.com>
2022-12-22 13:43:31 +0300

    Updating Dom content loaded

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//ADDING LINKS TO API
+const Meals =  'https://www.themealdb.com/api/json/v1/1/categories.php'
+const Drinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+const Search = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+
+//ADDIMG DOM CONTENT EVENT LISTENER TO LOAD CONTENT IN THE DOM
+document.addEventListener('DOMContentLoaded', () => {
+    
+})
